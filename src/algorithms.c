cf5b96ea2c6c5f19a4cdeb5e884d3addd3d06d87
taksh_404 <b24me1047@iitj.ac.in>
2025-04-13 01:09:50 +0530

    Update algorithms.c

--- src/algorithms.c
+++ src/algorithms.c
@@ -11,7 +11,7 @@ double haversine(double lat1, double lon1, double lat2, double lon2) {
     double dLon = (lon2 - lon1) * (M_PI / 180.0);
     lat1 = lat1 * (M_PI / 180.0);
     lat2 = lat2 * (M_PI / 180.0);
-    double a = sin(dLat / 2) * sin(dLat / 2) + sin(dLon / 2) * sin(dLon / 2) * cos(lat1) * cos(lat2);
+    double a = (sin(dLat / 2) * sin(dLat / 2))+(sin(dLon / 2) * sin(dLon / 2) * cos(lat1) * cos(lat2));
     double c = 2 * atan2(sqrt(a), sqrt(1 - a));
     return EARTH_RADIUS * c;
 }
