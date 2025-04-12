603344f6a8cb7ed54b5dd6e98d590cbfbc30d515
taksh_404 <b24me1047@iitj.ac.in>
2025-04-13 04:07:34 +0530

    Update warehouses.c

--- src/warehouses.c
+++ src/warehouses.c
@@ -1,31 +1,27 @@
 #include <stdio.h>
 #include "warehouses.h"
 
-int loadWarehouses(Warehouse warehouses[], const char *filename) {
+int loadWarehouses(Warehouse warehouses[], const char *filename)
+{
     FILE *fp = fopen(filename, "r");
-    if (!fp) {
+    if (!fp)
+    {
         printf("Error opening file: %s\n", filename);
         return 0;
     }
     int count = 0;
-    while (count < 50 &&
-           fscanf(fp, "%d,%49[^,],%99[^,],%lf,%lf,%d,%f\n",
-                  &warehouses[count].id,
-                  warehouses[count].name,
-                  warehouses[count].location,
-                  &warehouses[count].latitude,
-                  &warehouses[count].longitude,
-                  &warehouses[count].capacity,
-                  &warehouses[count].costPerUnit) == 7) {
+    while (count < 50 &&fscanf(fp, "%d,%49[^,],%99[^,],%lf,%lf,%d,%f\n",&warehouses[count].id,warehouses[count].name,warehouses[count].location,&warehouses[count].latitude,&warehouses[count].longitude,&warehouses[count].capacity,&warehouses[count].costPerUnit) == 7) {
         count++;
     }
     fclose(fp);
     return count;
 }
 
-void listWarehouses(Warehouse warehouses[], int count) {
+void listWarehouses(Warehouse warehouses[], int count)
+{
     printf("----- Warehouse List -----\n");
-    for (int i = 0; i < count; i++) {
+    for (int i = 0; i < count; i++)
+    {
         printf("ID: %d | Name: %s | Location: %s | Lat: %.4f | Lon: %.4f | Capacity: %d | Cost/Unit: $%.2f\n",
                warehouses[i].id, warehouses[i].name, warehouses[i].location,
                warehouses[i].latitude, warehouses[i].longitude,
