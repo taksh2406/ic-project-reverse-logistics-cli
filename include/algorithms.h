5012d00ae989e4749ed27c4e48df4c970b3c0744
taksh_404 <b24me1047@iitj.ac.in>
2025-04-13 00:40:54 +0530

    Update algorithms.h

--- include/algorithms.h
+++ include/algorithms.h
@@ -3,11 +3,8 @@
 
 #include "warehouses.h"
 
-// Existing functions
 double haversine(double lat1, double lon1, double lat2, double lon2);
 
-// New function: Dijkstra-based warehouse selection
-// Returns the index (in the warehouses array) for the best warehouse
 int dijkstraFindBestWarehouse(Warehouse warehouses[], int count, double productLat, double productLon);
 
-#endif // ALGORITHMS_H
+#endif 
