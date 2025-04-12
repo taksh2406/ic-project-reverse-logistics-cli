3e4aff43bb1619adbafd5b290e25991638fabd1f
taksh_404 <b24me1047@iitj.ac.in>
2025-04-13 01:56:15 +0530

    Update customers.c

--- src/customers.c
+++ src/customers.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include "customers.h"
 
-int loadCustomers(Customer customers[], const char *filename) {
+int loadCustomers(Customer customers[], const char *filename)
+{
     FILE *fp = fopen(filename, "r");
-    if (!fp) {
+    if (!fp)
+    {
         printf("Error opening file: %s\n", filename);
         return 0;
     }
     int count = 0;
-    while (count < 100 &&
-           fscanf(fp, "%d,%49[^,],%49[^,],%14s\n",
-                  &customers[count].id,
-                  customers[count].name,
-                  customers[count].email,
-                  customers[count].phone) == 4) {
+    while (count < 100 && fscanf(fp, "%d,%49[^,],%49[^,],%14s\n",&customers[count].id,customers[count].name,customers[count].email,customers[count].phone) == 4)
+    {
         count++;
     }
     fclose(fp);
     return count;
 }
 
-void listCustomers(Customer customers[], int count) {
+void listCustomers(Customer customers[], int count)
+{
     printf("----- Customer List -----\n");
-    for (int i = 0; i < count; i++) {
+    for (int i = 0; i < count; i++)
+    {
         printf("ID: %d | Name: %s | Email: %s | Phone: %s\n",
                customers[i].id, customers[i].name,
                customers[i].email, customers[i].phone);
