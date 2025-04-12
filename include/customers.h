484dc5237b410220ef56c0239ce9d4f4daeb0d48
taksh_404 <b24me1047@iitj.ac.in>
2025-04-13 00:43:13 +0530

    Update customers.h

--- include/customers.h
+++ include/customers.h
@@ -8,11 +8,8 @@ typedef struct {
     char phone[15];
 } Customer;
 
-// Loads customers from a CSV file.
-// Returns the number of records loaded.
-int loadCustomers(Customer customers[], const char *filename);
+int loadCustomers(Customer customers[],const char *filename);
 
-// Displays the list of customers.
-void listCustomers(Customer customers[], int count);
+void listCustomers(Customer customers[],int count);
 
-#endif // CUSTOMERS_H
+#endif 
